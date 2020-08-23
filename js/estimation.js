5928adc0ceffdfad2a58178494f76a18a5cfe412
Fly-76 <58425266+Fly-76@users.noreply.github.com>
2020-08-23 18:29:18 +0200

    implementation form contact

--- js/estimation.js
+++ js/estimation.js
@@ -14,10 +14,8 @@ Le coût est calculé de la manière suivante:
         brique: 30%
         bois: 15%
 Ainsi une maison de 50m2 avec deux chambres en brique est normalement estimée à: 91 000€
-
-A faire :
-    - controler les input; surface, chambre, matérieau
 */
+
 // ***********************************************************************************
 // Constant declaration
 const matArray = ["parpaing", "bois", "brique"];
@@ -31,8 +29,9 @@ let inputEstimation = document.getElementsByTagName("input")[3];
 let btnDropDown = document.getElementById("btnDropDown");
 let matériau = document.getElementsByClassName("dropdown-item");
 
-// Desactivate input: estimation, to avoid user action
+// Desactivate input: estimation, materiau to avoid user action
 inputEstimation.disabled = true;
+inputMateriau.disabled = true;
 
 // ***********************************************************************************
 // Compute estimation
@@ -128,98 +127,3 @@ for(let i=0; i<matériau.length; i++) {
         inputMateriau.value = this.innerText;
     })
 }
-
-
-
-
-
-
-
-
-    // 6c757d - secondary
-    // dc3545 - danger
-    // 28a745 - success
-    //  /^(?=.*[a-z])(?=.*\d).{6,30}$/i
-
-
-    // rgb(73, 80, 87)
-
-    // this.style.color = "red";
-    // #495057; default
-
-    // #6c757d
-
-    // 7f7f7f - black-50
-    // 343a40 - dark
-    // 212529 - body
-    // 6c757d - muted
-
-    // 6c757d - secondary
-    // dc3545 - danger
-    // 28a745 - success
-
-    // 721c24
-    // f8d7da
-
-
-    // --blue: #007bff;
-    // --indigo: #6610f2;
-    // --purple: #6f42c1;
-    // --pink: #e83e8c;
-    // --red: #dc3545;
-    // --orange: #fd7e14;
-    // --yellow: #ffc107;
-    // --green: #28a745;
-    // --teal: #20c997;
-    // --cyan: #17a2b8;
-    // --white: #fff;
-    // --gray: #6c757d;
-    // --gray-dark: #343a40;
-    // --primary: #007bff;
-    // --secondary: #6c757d;
-    // --success: #28a745;
-    // --info: #17a2b8;
-    // --warning: #ffc107;
-    // --danger: #dc3545;
-    // --light: #f8f9fa;
-    // --dark: #343a40;    
-
-//    console.log(window.getComputedStyle(this, null).getPropertyValue("color"));
-
-
-
-    // // if (!this.className.match(/(?:^|\s)text-danger(?!\S)/))
-    //     this.className += "text-danger";
-
-
-
-
-// this.className += "text-danger";
-// document.getElementById("monElement").className =
-//    document.getElementById("maClasse").className.replace( /(?:^|\s)MyClass(?!\S)/g , "" )
-
-// Cette expressions régulière permet de supprimer les occurrence d’un mot (maClasse) dans une liste de mots séparé par des espaces. En s’assurant de retirer les espaces. La même classe permet de savoir si une classe est déjà présente :
-
-// if ( document.getElementById("monElement").className.match(/(?:^|\s)maClasse(?!\S)/) )
-
-
-
-
-
-
-
-//if (event.key.match(/^[a-z0-9àéêè']+$/i)) {console.log("ok");}
-
-//     if (event.keyCode === 13) {
-
-// Commençons par la regex et admettons qu'elle permette de rechercher les numéros français, commençant par 0 ou +33 et possédant 9 chiffres sans compter le 0 ou +33. Ainsi, voici une écriture possible de celle-ci :
-
-// (0|\+33)[1-9]( *[0-9]{2}){4}
-
-// email Pour respecter la spécification HTML5 cf:developer.mozilla.org
-//var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-//<textarea  name=t2 id=t2 cols=25 rows=2 style="width:300px;"
-// onkeypress="if(this.value.match(/\D/)) this.value=this.value.replace(/\D/g,'')"
-// onkeyup   ="if(this.value.match(/\D/)) this.value=this.value.replace(/\D/g,'')"
-
